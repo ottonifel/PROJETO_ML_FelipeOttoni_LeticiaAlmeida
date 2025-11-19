15d227a092095723f654d7a31e0b50b7e5e73a85
Leticia Almeida <leticiapaulinoferreira@gmail.com>
2025-11-18 21:49:02 -0300

    cabeçalhos e def das funcoes de analise exploratoria

---
+++ scripts/__init__.py
@@ -0,0 +1,4 @@
+from .analise_exploratoria import analise_ACC, analise_BVP, analise_EDA, analise_HR, analise_IBI, analise_TEMP
+from .preprocessamento import preprocessar_dados
+from .experimentos import extrair_features
+from .analise_resultados import analisar_resultados
--- scripts/analise_exploratoria.py
+++ scripts/analise_exploratoria.py
@@ -7,11 +7,38 @@
 # Prof. Tiago A. Almeida
 #
 #
-# Nome:
-# RA:
+# Nome: Felipe Ottoni Pereira e Letícia Almeida Paulino de Alencar Ferreira
+# RA: 804317 (Felipe) e 800408 (Letícia)
 # ################################################################
 
 # Arquivo com todas as funcoes e codigos referentes a analise exploratoria
+
+
+# IBI: Cacular média, STD (desvio padrão) e RMSSD(FORMULA ESTRANHA), Q_1/Q_3 (Quartis) 25 e 75 quartil
+# ACC: Média de Magnitude (MÉDIA(raiz(X^2 + Y^2 + Z^2))), STD em [X, Y e Z], media de cada um (?)
+# HR: Média, STD (desvio pad), Média de Amplitude (Picos de BVP - Vales de BVP)
+# TEMP: Média, Range (MAX - MIN)
+# EDA: Média, STD(desvio padrão), SCR Rate (Número de Picos/Duração da Sessão)
+# BVP: Média, STD (desvio pad), Média de Amplitude (Picos de BVP - Vales de BVP)
+
+def analise_ACC():
+    pass
+
+def analise_BVP():
+    pass
+
+def analise_EDA():
+    pass
+
+def analise_HR():
+    pass
+
+def analise_IBI():
+    pass
+
+def analise_TEMP():
+    pass
+
 '''
 # Para cada sensor
     for sensor in sensors:
@@ -44,12 +71,6 @@ df_metrics = pd.DataFrame(summary_rows)
 # Fazer merge com df_users_info
 #df_final = df_users_info.merge(df_metrics, on="Id", how="left")'''
 
-# IBI: Cacular média, STD (desvio padrão) e RMSSD(FORMULA ESTRANHA), Q_1/Q_3 (Quartis) 25 e 75 quartil
-# ACC: Média de Magnitude (MÉDIA(raiz(X^2 + Y^2 + Z^2))), STD em [X, Y e Z], media de cada um (?)
-# HR: Média, STD (desvio pad), Média de Amplitude (Picos de BVP - Vales de BVP)
-# TEMP: Média, Range (MAX - MIN)
-# EDA: Média, STD(desvio padrão), SCR Rate (Número de Picos/Duração da Sessão)
-# BVP: Média, STD (desvio pad), Média de Amplitude (Picos de BVP - Vales de BVP)
 
 
 # Tags: para cada medida descobrir a faixa dos valores dos sensores que foram utilizados no experimento controlado de acordo com o tempo em tags, nas tabelas dos sensores e nas frequência de cada sensor
